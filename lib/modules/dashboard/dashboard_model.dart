e6408fddfd2bb27bd14b796d492a2a4addcde2f5
prasasdi <rizky.adie7@gmail.com>
2024-08-17 02:38:39 +0700

    ga usah pakai base T apa segala macam dulu

---
+++ lib/modules/dashboard/dashboard_model.dart
@@ -0,0 +1,21 @@
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/serializer.dart';
+
+// part 'dashboard_model.g.dart';
+
+class DashboardModel {
+  final String id;
+  final String title;
+  final int count;
+
+  DashboardModel({
+    required this.id,
+    required this.title,
+    required this.count,
+  });
+
+  
+}
+
---
+++ lib/modules/dashboard/dashboard_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class DashboardViewModel extends GetxController
+{
+  
+}
